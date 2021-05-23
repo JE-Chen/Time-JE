884d4619df28892f539d8f8655d4c1296f708670
JE-Chen <zenmailman@gmail.com>
2021-05-23 09:11:12 +0800

    refactor everything

--- Test/Time_Test.py
+++
@@ -1,9 +0,0 @@
-from Core.Time_Core import Time_Core
-Time1=Time_Core()
-print(Time1.Time_difference.Get_Now_Year())
-print(Time1.Time_difference.Get_Now_Mon())
-print(Time1.Time_Calendar.Get_Month_Calendar())
-print(Time1.Time_Calendar.Get_Year_Calendar())
-
-
-
